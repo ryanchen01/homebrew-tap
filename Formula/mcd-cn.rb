fa476fa9d49267f114780de69fb499b3984cb87d
ryanchen01 <chenjunhua1997@gmail.com>
2026-01-17 14:42:52 +0800

    Fix download URL format in update formula workflow

--- Formula/mcd-cn.rb
+++ Formula/mcd-cn.rb
@@ -6,20 +6,20 @@ class McdCn < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn-macos-universal.tar.gz"
+      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn_darwin_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn-macos-universal.tar.gz"
+      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn_darwin_amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn-macos-universal.tar.gz"
+      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn_linux_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn-macos-universal.tar.gz"
+      url "https://github.com/ryanchen01/mcd-cn/releases/download/0.1.1/mcd-cn_linux_amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
